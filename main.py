066170a652a2dd090cde61a4aca8c4d9d8c4ddba
ivnxyz <ivanmartinez33@outlook.com>
2020-05-11 18:18:31 -0500

    Se agregó código en el archivo main

---
+++ main.py
@@ -0,0 +1,13 @@
+# Importar dependencias
+from tweepy_wrapper import TweepyWrapper
+from mentions_listener import MentionsListener
+from credentials import CREDENTIALS
+
+# Crear instancia de TweepyWrapper
+tweepy_wrapper = TweepyWrapper(CREDENTIALS['CONSUMER_KEY'], CREDENTIALS['CONSUMER_SECRET'], CREDENTIALS['ACCESS_KEY'], CREDENTIALS['ACCESS_SECRET'])
+
+# Escuchar las menciones
+mentions_listener = MentionsListener(tweepy_wrapper)
+
+stream = tweepy.Stream(tweepy_wrapper.api.auth, mentions_listener)
+stream.filter(track=['@MemefyBot'])
